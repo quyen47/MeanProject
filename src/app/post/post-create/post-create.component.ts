076405b9253e308de02dde93f4a87eb7b8e91bcd
Nguyen Ngoc Quyen <nnquyen47@gmail.com>
2018-10-08 17:11:39 +0700

    update generic Post for EventEmitter

--- src/app/post/post-create/post-create.component.ts
+++ src/app/post/post-create/post-create.component.ts
@@ -9,7 +9,7 @@ import { Post } from '../post.model';
 export class PostCreateComponent {
   enteredContent = '';
   enteredTitle = '';
-  @Output() postCreated = new EventEmitter();
+  @Output() postCreated = new EventEmitter<Post>();
 
   onAddPost() {
     const post: Post = {
