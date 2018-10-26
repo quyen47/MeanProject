e89fe2203cd86c773013d9fad2f4d40ccfc00c6a
Nguyen Ngoc Quyen <nnquyen47@gmail.com>
2018-10-26 16:26:36 +0700

    using module routing for create and root page

---
+++ src/app/app-routing.model.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { PostListComponent } from './post/post-list/post-list.component';
+import { PostCreateComponent } from './post/post-create/post-create.component';
+
+const routers = [
+    { path: '', component: PostListComponent},
+    { path: 'create', component: PostCreateComponent}
+];
+@NgModule({
+    imports: [RouterModule.forRoot(routers)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule {
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { AppComponent } from './app.component';
 import { PostCreateComponent } from './post/post-create/post-create.component';
 import { HeaderComponent } from './header/header.component';
 import { PostListComponent } from 'src/app/post/post-list/post-list.component';
+import { AppRoutingModule } from './app-routing.model';
 
 @NgModule({
   declarations: [
@@ -19,6 +20,7 @@ import { PostListComponent } from 'src/app/post/post-list/post-list.component';
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
     FormsModule,
     BrowserAnimationsModule,
     MatInputModule,
