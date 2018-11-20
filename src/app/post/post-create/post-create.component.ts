50bc50d2db0d20b131c866621837e6fa22f84f56
Nguyen Ngoc Quyen <nnquyen47@gmail.com>
2018-11-20 14:52:23 +0700

    add button upload image and instead FormsModule by ReactiveFormsModule

--- src/app/post/post-create/post-create.component.ts
+++ src/app/post/post-create/post-create.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit} from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { NgForm, FormGroup, FormControl, Validators } from '@angular/forms';
 import { PostsService } from '../posts.service';
 import { ActivatedRoute, ParamMap } from '@angular/router';
 import { Post } from '../post.model';
@@ -15,10 +15,19 @@ export class PostCreateComponent implements OnInit {
   post: Post;
   mode = 'create';
   private isLoading = false;
+  postForm: FormGroup;
 
   constructor(public postsService: PostsService, public router: ActivatedRoute) {}
 
   public ngOnInit() {
+    this.postForm = new FormGroup ({
+      title: new FormControl(null, {
+        validators: [Validators.required, Validators.minLength(3)]
+      }),
+      content: new FormControl(null, {
+        validators: [Validators.required, Validators.minLength(3)]
+      })
+    });
     this.router.paramMap.subscribe((paramMap: ParamMap) => {
       if (paramMap.has('postId')) {
         this.mode = 'edit';
@@ -29,6 +38,7 @@ export class PostCreateComponent implements OnInit {
           .subscribe(postData => {
             this.isLoading = false;
             this.post = {id: postData._id, title: postData.title, content: postData.content};
+            this.postForm.setValue({'title': this.post.title, 'content': this.post.content});
           });
       } else {
         this.mode = 'create';
@@ -37,16 +47,16 @@ export class PostCreateComponent implements OnInit {
     });
   }
 
-  onAddPost(param: NgForm) {
-    if (param.invalid) {
+  onAddPost() {
+    if (this.postForm.invalid) {
       return;
     }
     if (this.mode === 'create') {
-      this.postsService.addPost(param.value.title, param.value.content);
+      this.postsService.addPost(this.postForm.value.title, this.postForm.value.content);
     } else {
-      this.postsService.updatePost(this.postId, param.value.title, param.value.content);
+      this.postsService.updatePost(this.postId, this.postForm.value.title, this.postForm.value.content);
     }
-    param.resetForm();
+    this.postForm.reset();
   }
 
 }
